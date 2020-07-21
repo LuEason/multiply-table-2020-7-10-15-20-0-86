693207c8e62104cdf11139865676c6654d0a31f0
Eason <chenzscd@163.com>
2020-07-21 20:55:24 +0800

    [Implement A Multiplication Table] [Eason] finish

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 function createMultiplyTable(start, end) {
+  if (isNumbersValid(start, end)) {
+    return printAllLines(start, end);
+  }
   return null;
+}
+
+function isNumbersValid(start, end) {
+  return start <= end && start >=1 && start <= 1000 && end >=1 && end <= 1000;
+}
+
+function printAllLines(start, end) {
+  let output = [];
+  for (let i = start; i <= end; i++) {
+    output.push(printOneLine(start, i));
+  }
+  return output.join('\n');
+}
+
+function printOneLine(start, end) {
+  let output = [];
+  for (let i = start; i <= end; i++) {
+    output.push(printAMultiply(i, end));
+  }
+  return output.join('\t');
+}
+
+function printAMultiply(multiplicand, multiplier) {
+  return multiplicand + '*' + multiplier + '=' + (multiplicand * multiplier);
 }
 
 module.exports = {
